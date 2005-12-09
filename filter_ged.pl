a023d5495d002e4eb9b4dbdd614efd10f9e68a34
Claus Schrammel <claus@f05fk.net>
2005-12-09 19:37:56 +0100

    filtering family Schrammel.

--- filter_ged.pl
+++ filter_ged.pl
@@ -21,4 +21,118 @@ use strict;
 use GED;
 
 my $ged = new GED($ARGV[0]);
+
+my $individual = $ged->{individuals}->{"I3"};
+$individual->{keep} = 1;
+&keep_family("F2");
+&remove_entries();
+
 $ged->save($ARGV[1]);
+
+exit 0;
+
+sub keep_family
+{
+    my $family_id = shift;
+    my $family = $ged->{families}->{$family_id};
+
+    # exit if not exists
+    return if (!$family);
+
+    # exit if already visited
+    return if ($family->{keep});
+
+    $family->{keep} = 1;
+
+    &keep_individual($family->{husband});
+    &keep_individual($family->{wife});
+
+    foreach my $child (keys %{$family->{children}})
+    {
+        &keep_individual($child);
+    }
+}
+
+sub keep_individual
+{
+    my $individual_id = shift;
+    my $individual = $ged->{individuals}->{$individual_id};
+
+    # exit if not exists
+    return if (!$individual);
+
+    # exit if already visited
+    return if ($individual->{keep});
+
+    $individual->{keep} = 1;
+
+    &keep_family($individual->{family_child});
+
+    foreach my $family_spouse (keys %{$individual->{family_spouse}})
+    {
+        &keep_family($family_spouse);
+    }
+}
+
+sub remove_entries
+{
+    foreach my $family_id (keys %{$ged->{families}})
+    {
+        my $family = $ged->{families}->{$family_id};
+
+        if ($family->{keep})
+        {
+            my $husband = $ged->{individuals}->{$family->{husband}};
+            if (!$husband || !$husband->{keep})
+            {
+                delete $family->{husband};
+            }
+
+            my $wife = $ged->{individuals}->{$family->{wife}};
+            if (!$wife || !$wife->{keep})
+            {
+                delete $family->{wife};
+            }
+
+            foreach my $child_id (keys %{$family->{children}})
+            {
+                my $child = $ged->{individuals}->{$child_id};
+                if (!$child || !$child->{keep})
+                {
+                    delete $family->{children}->{$child_id};
+                }
+            }
+        }
+        else
+        {
+            delete $ged->{families}->{$family_id};
+        }
+    }
+
+    foreach my $individual_id (keys %{$ged->{individuals}})
+    {
+        my $individual = $ged->{individuals}->{$individual_id};
+
+        if ($individual->{keep})
+        {
+            my $family_child = $ged->{families}->{$individual->{family_child}};
+            if (!$family_child || !$family_child->{keep})
+            {
+                delete $individual->{family_child};
+            }
+
+            foreach my $family_spouse_id (keys %{$individual->{family_spouse}})
+            {
+                my $family_spouse = $ged->{families}->{$family_spouse_id};
+                if (!$family_spouse || !$family_spouse->{keep})
+                {
+                    delete $individual->{family_spouse}->{$family_spouse_id};
+                }
+            }
+        }
+        else
+        {
+            delete $ged->{individuals}->{$individual_id};
+        }
+    }
+}
